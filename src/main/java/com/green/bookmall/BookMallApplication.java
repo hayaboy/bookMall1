76580f76ec18e1de07ae317e9c5f736e1f0ce3cf
hayaboy <hayaboy@korea.ac.kr>
2024-04-04 17:14:46 +0900

    Initial commit

---
+++ src/main/java/com/green/bookmall/BookMallApplication.java
@@ -0,0 +1,13 @@
+package com.green.bookmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMallApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/green/bookmall/TestController.java
@@ -0,0 +1,2 @@
+package com.green.bookmall;public class TestController {
+}
